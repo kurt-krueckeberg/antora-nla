ef4e63f66255663dc1e6352a6a68c4186b57b7a0
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2025-07-21 15:26:53 -0400

    $1

---
+++ modules/1237/pages/index.php
@@ -0,0 +1,7 @@
+= Document NLA BU, K 2, A Nr. 1237
+
+The document denotated as *NLA BU, K 2, A Nr. 1237* is composed of 12 images received from the Niedersächishces Landesarchiv,
+downloaded TIF images, each about 58MG in size. It is one of five other documents and other research containted in a downloadable
+.zip file. 
+
+Pages with the transliteration and translation of each image will be added over time.
